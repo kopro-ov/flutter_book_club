93580b689f586123dee2fd1eeb61d43ac3d641bb
kopro-ov <koprodev@gmail.com>
2022-03-22 23:04:26 +0900

    search FittedBox class

--- lib/search.dart
+++ lib/search.dart
@@ -3,11 +3,32 @@ import 'package:flutter/material.dart';
 class Search extends StatelessWidget {
   const Search({Key? key}) : super(key: key);
 
+  static const String _title = '검색페이지';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('검색 페이지'),
+      appBar: AppBar(title: const Text(_title)),
+      body: const Center(
+        child: MyStatelessWidget(),
+      ),
+    );
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 400,
+      width: 300,
+      color: Colors.red,
+      child: FittedBox(
+        fit: BoxFit.fill,
+        child: Image.network(
+            'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'),
       ),
     );
   }
